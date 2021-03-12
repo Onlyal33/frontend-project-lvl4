3477a74f3b17f2428f717470a7adbf91f14e132d
onlyal33 <alex.lapygin@gmail.com>
2021-03-12 17:57:30 +0300

    Messages component code refactoring

--- src/features/messages/Messages.jsx
+++ src/features/messages/Messages.jsx
@@ -11,13 +11,13 @@ const renderMessage = ({ id, nickname, message }) => (
   </span>
 );
 
-const Messages = () => {
-  const selectMessagesByChannelId = (state) => {
-    const { currentChannelId } = state;
-    return state.messages
-      .filter(({ channelId }) => channelId === currentChannelId);
-  };
+const selectMessagesByChannelId = (state) => {
+  const { currentChannelId } = state;
+  return state.messages
+    .filter(({ channelId }) => channelId === currentChannelId);
+};
 
+const Messages = () => {
   const filteredMessages = useSelector(selectMessagesByChannelId, shallowEqual);
 
   if (filteredMessages.length === 0) {
