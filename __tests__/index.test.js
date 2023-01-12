3c51d31a1093c5f2652dd4d8792b3f43c7e66a76
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-01-12 22:05:17 +0100

    fixed logger options in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,16 @@ import { test, expect } from '@jest/globals';
 import init from '../server/plugin.js';
 
 test('get /', async () => {
-  const app = fastify({ logger: { prettyPrint: true } });
+  const app = fastify({
+    logger: {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+        },
+      },
+    },
+  });
   await init(app, { state: {} });
   const response = await app.inject({
     url: '/',
